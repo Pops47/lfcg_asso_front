a2b3e6e2a2e8f06ab15f202f1abe024a70180539
Pauline SOUBRIE <pauline_soubrie@hotmail.fr>
2024-10-14 14:51:14 +0200

    debug

--- src/hooks/useApi.ts
+++ src/hooks/useApi.ts
@@ -4,7 +4,5 @@ export function useApi() {
   const api: AxiosInstance = axios.create({
     baseURL: import.meta.env.VITE_BACK_URL,
   });
-  console.log("import.meta.env", import.meta.env);
-
   return api;
 }
