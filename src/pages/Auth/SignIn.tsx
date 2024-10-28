9d1623b84dcfdc1d7f6f32169f476697a3088fca
Pauline SOUBRIE <pauline_soubrie@hotmail.fr>
2024-10-28 17:34:02 +0100

    fix: missing axios import

--- src/pages/Auth/SignIn.tsx
+++ src/pages/Auth/SignIn.tsx
@@ -9,6 +9,7 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { zodResolver } from "@hookform/resolvers/zod";
+import axios from "axios";
 import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
 import z from "zod";
