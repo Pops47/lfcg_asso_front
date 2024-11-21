b30b985ffc1d2be6e4d073868c2a9de42b88b421
Pauline SOUBRIE <pauline_soubrie@hotmail.fr>
2024-11-21 09:15:07 +0100

    test pipeline

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -2,6 +2,7 @@ import { Button } from "@/components/ui/button";
 import { Link } from "react-router-dom";
 
 export default function HomePage() {
+  console.log("VITE_API_BASE_URL", import.meta.env.VITE_API_BASE_URL);
   return (
     <div className="min-h-screen flex bg-gray-100 w-full">
       <main className="w-full relative">
